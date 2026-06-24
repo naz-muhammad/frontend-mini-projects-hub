50a7125d6dac9c80a88933739507d371984bfd00
naz-muhammad <nazmuhammad.dev@gmail.com>
2026-06-24 00:45:48 -0700

    complete the practice project of Background Color Changer

---
+++ 04-JavaScript Beginner Projects/02 - Background Color Changer/app.js
@@ -0,0 +1,24 @@
+const red = document.querySelector(".redbtn")
+const blue = document.querySelector(".bluebtn")
+const green = document.querySelector(".greenbtn")
+const yellow = document.querySelector(".yellowbtn")
+
+red.addEventListener("click" , () => {
+    document.body.style.backgroundColor = "red"
+    
+})
+
+blue.addEventListener("click" , () => {
+    document.body.style.backgroundColor = "blue"
+    
+})
+
+green.addEventListener("click" , () => {
+    document.body.style.backgroundColor = "green"
+    
+})
+
+yellow.addEventListener("click" , () => {
+    document.body.style.backgroundColor = "yellow"
+   
+})
