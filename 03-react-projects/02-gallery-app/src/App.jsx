f40ca7e370b8fee779f4ae24a03f417b9fb42b14
naz-muhammad <nazmuhammad.dev@gmail.com>
2026-08-03 10:35:21 -0700

    add prev and next button and set dynamic page changing

--- 03-react-projects/02-gallery-app/src/App.jsx
+++ 03-react-projects/02-gallery-app/src/App.jsx
@@ -3,30 +3,38 @@ import axios from 'axios'
 
 const App = () => {
 
+  
   const [data , setData] = useState([])
+  const [count , setCount] = useState(1)
+
+  // let count = 2;
+  
 
   useEffect(()=> {
     const getData = async () => {
 
-    const response = await axios.get('https://picsum.photos/v2/list?page=1&limit=10')
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${count}&limit=10`)
     // console.log(response.data);
       setData(response.data)
   }
 
   getData()
 
-  } , [])
+  } , [count])
 
-  console.log(data);
-  
+
 
   return (
 
-    <div className='h-full bg-neutral-50 text-white flex gap-4 flex-wrap justify-center p-8'>
+    <>
+
+    <div className='h-full bg-[#212121] text-white flex gap-4 flex-nowrapwrap justify-center items-center p-4 flex-col-reverse'>
 
       {
         data.map( ( user , idx ) => {
-          return <div key={idx} 
+        
+          return <a href={user.url}  key={idx} >
+            <div
             className='rounded-xl '
           >
            <img src={user.download_url} 
@@ -37,19 +45,32 @@ const App = () => {
            >{user.author}</h1>
            
           </div>
+          </a>
         })
       }
 
-      <div className='flex gap-4 mb-4'>
+      <div className='flex gap-4 my-4 '>
           <button
-            className='text-lg bg-amber-500 text-black px-6 py-4 rounded-2xl'
+            className='text-lg bg-amber-500 text-black px-6 py-4 rounded-2xl cursor-pointer'
+            onClick={ ()=> {
+              if ( count > 1 ) {
+                setCount(count-1)
+              }
+              
+            }}
            >Prev</button>
+           <h3 className='text-xl flex items-center'>{`page ${count}`}</h3>
            <button
-            className='text-lg bg-amber-500 text-black px-6 py-4 rounded-2xl'
+            className='text-lg bg-amber-500 text-black px-6 py-4 rounded-2xl '
+              onClick={ ()=> {
+              setCount((prev)=> prev+1)
+            }}
            >Next</button>
       </div>
       
     </div>
+    </>
+    
 
   )
 }
